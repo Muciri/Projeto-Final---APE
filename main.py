85ea67139463bf36753ea1d3e8c1f437660b86fc
Felipe Oliveira Raimundo <20241370007@ad.ifpb.edu.br>
2024-09-24 14:19:12 -0300

    segundo requisito feito

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+import pandas as pd
+
+df = pd.read_csv('consulta_cand_2024_PB.csv', encoding='latin1', delimiter=';')
+
+def buscar_candidato(num_candidato):
+    try:
+        candidato = df[df['SQ_CANDIDATO'] == int(num_candidato)]
+        if not candidato.empty:
+            return(f'''
+                Nome: {candidato['NM_CANDIDATO'].values[0]}
+                Nome na urna: {candidato['NM_URNA_CANDIDATO'].values[0]}
+                número: {candidato['NR_CANDIDATO'].values[0]}
+                partido: {candidato['NM_PARTIDO'].values[0]}
+                    ''')
+        else:
+            return('candidato não encontrado')
+    except:
+        return('candidato inválido')
+
+
 print('=-=-=-=-=-=-=-=-=-=-=-ELEIÇÕES 2024=-=-=-=-=-=-=-=-=-=-=-')
 
 #interface principal do programa
@@ -9,8 +29,8 @@ while True:
                 print('imprimindo lista de candidatos...')
                 print('opção ainda não disponível')
             case '2':
-                print('exibindo informações...')
-                print('opção ainda não disponível')
+                num_candidato = input('digite o número do candidato:')
+                print(buscar_candidato(num_candidato))
             case '3':
                 print('gerando página HTML...')
                 print('opção ainda não disponível')
